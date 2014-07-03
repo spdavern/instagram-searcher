ec0834b067c5a8e8ccd5b80e21c81becdb4bf425
spdavern <sean@lizandsean.com>
2014-07-03 03:44:48 -0700

    Initial completion of assignment.

--- scripts/myApp.js
+++ scripts/myApp.js
@@ -1,13 +1,59 @@
 angular.module('myApp', ["ngAnimate"])
-	.constant('VERSION', "0.1")
+	.constant('VERSION', "1.0")
 	.run(function(VERSION, $rootScope) {
 		$rootScope.version = VERSION;
 	})
-	.controller('instagramSearchCtrl', function($scope) {
-		$scope.data = {submitted: false}
+	.controller('instagramSearchCtrl', ['$scope', '$http', function($scope, $http) {
+		$scope.formData = {submitted: false, haveResults: false, fadeResults: false,
+						   message: ""};
+		$scope.imageData = {data: []};
 		$scope.submit = function() {
+			$scope.fadeResults = true;
 			if($scope.myForm.$valid) {
-				$scope.data.submitted = true;
+				//Clear the text field and report that instagram is being queried.
+				$scope.formData.searchingFor = $scope.formData.tag.trim();
+				$scope.formData.tag = "";
+				$scope.message = "Searching Instagram for photos tagged with " +
+									$scope.formData.searchingFor + "."
+				$scope.formData.submitted = true;
+				//query the instagram API for the tag.
+				$scope.getImages($scope.formData.searchingFor);
 			};
 		};
-	});
+	    $scope.getImages = function(tagRequest) {
+	    	var CLIENT_ID = "d8d33d939d66419c8bd2669014e7ef78"
+	        var url = "https://api.instagram.com/v1/tags/" + tagRequest + "/media/recent"
+	        var request = {
+	        	callback: 'JSON_CALLBACK',
+	        	client_id: CLIENT_ID
+	        };
+	        $http({
+	            method: 'JSONP',  //This JSONP method tells Angular to use JSONP for the GET.
+	            url: url,
+	            params: request
+	        }).success(function(results) {
+	        	if (results.meta.code == 200) {
+	        		if (results.data.length > 0) {
+	        			$scope.haveResults = true;
+	        			$scope.fadeResults = false;
+	        			$scope.images = results.data
+	        			$scope.message = 'We found ' + results.data.length +
+	        							 ' results for "' + $scope.formData.searchingFor + '"'
+	        		} else {
+	        			$scope.message = 'No images were found for the tag "' + 
+	        								$scope.formData.searchingFor + '".'
+	        			$scope.fadeResults = true
+	        		}
+	        	} else {
+	        		$scope.message = 'Searched for "' + $scope.formData.searchingFor 
+	        							+ '".  ' + results.meta.error_message;
+	        		$scope.fadeResults = true;
+	        	}
+	        }).error(function() {
+	            alert('The Instagram search for "' + tagRequest + 
+	            	  '" was not successful.  Only one tag, please. (no spaces)');
+	            $scope.message = "";
+	            $scope.fadeResults = true;
+	        })
+	    };
+	}]);
